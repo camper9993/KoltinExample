8b0e0aa0a326d64d7c97f094e879da9ac109c005
camper9993 <danilov_1999@mail.ru>
2020-07-28 10:27:12 +0500

    Second

--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -7,20 +7,24 @@ object UserHolder  {
 
     fun registerUser(fullName:String, email : String, password : String) : User {
         return User.makeUser(fullName, email, password).also {
-            require(!map.contains(it.login)) { println("A user with this email already exists")}
-            map[it.login] = it
+            if (map.contains(it.login)) {
+                throw IllegalArgumentException("A user with this email already exists")
+            }
+            else
+                map[it.login] = it
         }
     }
 
 
 
     fun registerUserByPhone(fullName: String, rawPhone: String) : User {
-        if (rawPhone.matches("\\+\\d \\(\\d{3}\\) \\d{3}-\\d{2}-\\d{2}".toRegex()))
+        if (rawPhone.trimPhone().matches("\\+\\d{11}".toRegex()))
             return User.makeUser(fullName, null, null, rawPhone.trimPhone()).also {
                 require(!map.contains(it.login)) { println("A user with this phone already exists")}
                 map[it.login] = it
             }
-        else throw IllegalArgumentException("Enter a valid phone number starting with a + and containing 11 digits")
+        else
+            throw IllegalArgumentException("Enter a valid phone number starting with a + and containing 11 digits")
     }
 
     fun loginUser (login : String, password: String) : String? {
